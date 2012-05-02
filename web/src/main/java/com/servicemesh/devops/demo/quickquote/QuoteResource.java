8c42312f090dff2664831a3790a23aacccdd7f29
Kyle Falkenhagen <kyle.falkenhagen@servicemesh.com>
2012-05-01 19:46:59 -0500

    Remove Kyle as an Option

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -36,7 +36,6 @@ public class QuoteResource {
 		insuranceTypes.add("Marine");
 		//insuranceTypes.add("Banana");	
 		insuranceTypes.add("Life");
-		insuranceTypes.add("Kyle");
 		return insuranceTypes;
 	}
 
