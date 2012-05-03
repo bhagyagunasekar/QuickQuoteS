b27fb7cb05fb16fa1437b5d44798c1fa3173bc63
Henry Stapp <hstapp@gmail.com>
2012-05-03 09:19:48 -0700

    We insure watermelons.

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -35,6 +35,7 @@ public class QuoteResource {
 		insuranceTypes.add("Home");
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
+		insuranceTypes.add("Watermelon")
 		return insuranceTypes;
 	}
 
