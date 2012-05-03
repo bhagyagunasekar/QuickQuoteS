b734e042a2b50836fa87125a7fa12a3bf816cc61
Henry Stapp <hstapp@gmail.com>
2012-05-03 09:27:49 -0700

    fixed bug

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -35,7 +35,7 @@ public class QuoteResource {
 		insuranceTypes.add("Home");
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
-		insuranceTypes.add("Watermelon")
+		insuranceTypes.add("Watermelon");
 		return insuranceTypes;
 	}
 
