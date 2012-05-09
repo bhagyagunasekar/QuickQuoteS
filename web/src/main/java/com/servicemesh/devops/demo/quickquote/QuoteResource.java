ca85df974c620babdfd187a5893d14485547bc79
Henry Stapp <hstapp@gmail.com>
2012-05-08 18:19:09 -0700

    Added marine.

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -33,8 +33,8 @@ public class QuoteResource {
 		List<String>insuranceTypes = new ArrayList<String>();
 		insuranceTypes.add("Auto");
 		insuranceTypes.add("Home");
-		//insuranceTypes.add("Marine");
-		insuranceTypes.add("TEOTWAWKI");
+		insuranceTypes.add("Marine");
+		//insuranceTypes.add("TEOTWAWKI");
 		return insuranceTypes;
 	}
 
