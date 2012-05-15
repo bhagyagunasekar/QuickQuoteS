d94beae655fd60c84e09ee3a415ed685e4eb5920
Henry Stapp <hstapp@gmail.com>
2012-05-15 13:47:32 -0700

    Added Beaver.

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -35,7 +35,7 @@ public class QuoteResource {
 		insuranceTypes.add("Home");
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
-		insuranceTypes.add("Bee");
+		insuranceTypes.add("Beaver");
 		return insuranceTypes;
 	}
 
