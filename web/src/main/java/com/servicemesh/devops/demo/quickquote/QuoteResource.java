f3f341734d521aadcd7848ecdc414490c358b97e
Henry Stapp <hstapp@gmail.com>
2012-05-14 17:21:01 -0700

    Removed cat.

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -35,7 +35,7 @@ public class QuoteResource {
 		insuranceTypes.add("Home");
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
-		insuranceTypes.add("Cat");
+		//insuranceTypes.add("Cat");
 		return insuranceTypes;
 	}
 
