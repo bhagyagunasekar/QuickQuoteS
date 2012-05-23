aaf51f2a58905518ff87eafbf0f6a32c644b3517
Henry Stapp <hstapp@gmail.com>
2012-05-23 09:51:26 -0700

    Added Antelope.

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -33,7 +33,7 @@ public class QuoteResource {
 		List<String>insuranceTypes = new ArrayList<String>();
 		insuranceTypes.add("Auto");
 		insuranceTypes.add("Home");
-		insuranceTypes.add("Marine");
+		insuranceTypes.add("Antelope");
 		insuranceTypes.add("Life");
 		return insuranceTypes;
 	}
