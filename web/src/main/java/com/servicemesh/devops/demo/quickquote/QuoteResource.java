0cc940a714a60d6aef854ad4af6f1a66f63bf2ab
Henry Stapp <hstapp@gmail.com>
2012-09-28 11:52:55 -0700

    Llama -> Banana

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -35,7 +35,7 @@ public class QuoteResource {
 		insuranceTypes.add("Home");
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
-		insuranceTypes.add("Llama");
+		insuranceTypes.add("Banana");
 		return insuranceTypes;
 	}
 
