b9e278d81da707b6859ce1ffd27cb45bbb67c388
jdai <jdai@csc.com>
2014-09-30 10:08:13 -0500

    aa

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -35,7 +35,7 @@ public class QuoteResource {
 		insuranceTypes.add("Home");
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
-		insuranceTypes.add("Test1");
+		insuranceTypes.add("Demo51");
 		insuranceTypes.add("Orion");
 		return insuranceTypes;
 	}
