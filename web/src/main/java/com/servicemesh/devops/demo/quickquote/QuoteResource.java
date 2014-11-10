8c5e859253b5f5065fda30f4e85d895715768bc8
mdutton5 <mdutton5@mdutton5-mac.home>
2014-11-10 19:21:42 +0000

    Removed pet

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -36,7 +36,7 @@ public class QuoteResource {
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
 //		insuranceTypes.add("Demo51");
-		insuranceTypes.add("Pet");
+//		insuranceTypes.add("Pet");
 		return insuranceTypes;
 	}
 
