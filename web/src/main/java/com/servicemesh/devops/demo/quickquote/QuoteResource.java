b926f0e691f0ee2ddf0c83d36e9287ff9bfca0ab
mdutton5 <dutton.matt@gmail.com>
2014-11-19 10:01:12 +0000

    Added Travel

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -35,7 +35,7 @@ public class QuoteResource {
 		insuranceTypes.add("Home");
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
-//		insuranceTypes.add("Travel");
+		insuranceTypes.add("Travel");
 //		insuranceTypes.add("Demo51");
 		return insuranceTypes;
 	}
