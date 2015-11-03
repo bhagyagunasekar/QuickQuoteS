01beb890fa5695b110e45fe3852194f2286cb160
Bhagyagunasekar <bgunasekar2>
2015-11-03 22:26:34 +0530

    test

--- web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
+++ web/src/main/java/com/servicemesh/devops/demo/quickquote/QuoteResource.java
@@ -32,7 +32,7 @@ public class QuoteResource {
 	private List<String> getInsuranceTypeData() {
 		List<String>insuranceTypes = new ArrayList<String>();
 		insuranceTypes.add("Auto");
-		insuranceTypes.add("Home");
+	//	insuranceTypes.add("Home");
 		insuranceTypes.add("Marine");
 		insuranceTypes.add("Life");
 		insuranceTypes.add("Travel");
